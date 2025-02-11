8580ee21c4dfb727e188a2852a5c8fb5eb5e0d2b
VladTca <votsats@gmail.com>
2025-02-11 20:33:42 +0100

    Refactor Sender class: consolidate mail sending logic and simplify main method

--- src/Sender.java
+++ src/Sender.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Sender {
 
-    public void send(MailDeliveryService mailDeliveryService){
+    static void sendingMail(List<MailDeliveryService> services) {
+      send(choiceService(services));
+    }
+
+    static List<MailDeliveryService> getMailDeliveryServices() {
+        List<MailDeliveryService> services = new ArrayList<>();
+        services.add(new DHL());
+        services.add(new Email());
+        services.add(new Pigeon());
+        return services;
+    }
+
+    public static MailDeliveryService choiceService(List<MailDeliveryService> services ) {
+        System.out.println("Какую службу доставки использовать? (Pigeon, Email, DHL)");
+        Scanner sc = new Scanner(System.in);
+        String deliveryService = sc.nextLine();
+        sc.close();
+        for (MailDeliveryService service : services) {
+            if (service.getClass().getSimpleName().equalsIgnoreCase(deliveryService)) {
+                return service;
+            }
+        }
+        return new MailDeliveryService() {
+        };
+    }
+
+    public static void send(MailDeliveryService mailDeliveryService){
         mailDeliveryService.sendMail();
     }
 }
